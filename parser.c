43c4ef69f21e55bdfc045a2612efb6f1b88edd12
Michal Šlesár <uarebugged@gmail.com>
2020-11-18 22:31:07 +0100

    IFJ-15 Change types

--- parser.c
+++ parser.c
@@ -21,7 +21,7 @@ Stack *stack;
 
 #define GET_TOKEN_AND_COMPARE_ERROR(token_type)\
     RETRIEVE_TOKEN();\
-    if(token.type != token_type) THROW_SYNTAX();
+    if(token.type != token_type) THROW_ERROR_SYNTAX();
 
 #define GET_TOKEN_AND_COMPARE_RETURN(token_type)\
     RETRIEVE_TOKEN();\
@@ -113,69 +113,97 @@ void ruleFunc() {
     GET_TOKEN_AND_COMPARE_ERROR(TOKEN_BRACE_LEFT);
     GET_TOKEN_AND_COMPARE_ERROR(TOKEN_EOL);
     GET_TOKEN_AND_COMPARE_ERROR(TOKEN_BRACE_RIGHT);
+
+    /*Token actual_token;
+
+    if(!stackIsEmpty(stack))
+        actual_token = stackPop(stack);
+    else
+        actual_token = getToken();
+
+    if ((actual_token.type == TOKEN_KEYWORD) && (actual_token.value.k == KEYWORD_FUNC)) {
+        actual_token = getToken();
+        if (actual_token.type == TOKEN_IDENTIFIER) {
+            actual_token = getToken();
+            if (actual_token.type == TOKEN_BRACKET_LEFT) {
+                actual_token = getToken();
+                if (actual_token.type == TOKEN_BRACKET_RIGHT) {
+                    actual_token = getToken();
+                    if (actual_token.type == TOKEN_BRACE_LEFT) {
+                        actual_token = getToken();
+                        if (actual_token.type == TOKEN_EOL) {
+                            actual_token = getToken();
+                            if (actual_token.type == TOKEN_BRACE_RIGHT) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;*/
 }
 
 /*  6: <params> -> id <type> <params_n>
     7: <params> -> eps  */
-//bool ruleParams() 
+//void ruleParams() 
 
 /*  8: <params_n> -> , id <type> <params_n>
     9: <params_n> -> eps */
-//bool ruleParamsN()
+//void ruleParamsN()
 
 /*  10: <type> -> int
     11: <type> -> float64
     12: <type> -> string
     13: <type> -> eps */
-//bool ruleType()
+//void ruleType()
 
 /*  14: <ret_types> -> ( <type> <ret_types_n> )
     15: <ret_types> -> eps */
-//bool ruleRetTypes()
+//void ruleRetTypes()
 
 /*  16: <ret_types_n> -> , <type> <ret_types_n>
     17: <ret_types_n> -> eps */
-//bool ruleRetTypesN()
+//void ruleRetTypesN()
 
 /*  18: <st_list> -> <stat> EOL <st_list>
     19: <st_list> -> eps */
-//bool ruleStList()
+//void ruleStList()
 
 /*  20: <stat> -> id <stat_body>
     30: <stat> -> if <expression> { <st_list> } else { <st_list> }
     31: <stat> -> for <stat> ; <expression> ; <stat> { <st_list> }
     32: <stat> -> <return> */
-//bool ruleStat()
+//void ruleStat()
 
 /*  21: <stat_body> -> <id_n> = <assign>
     24: <stat_body> -> := <expression>
     25: <stat_body> -> ( <params> ) */
-//bool ruleStatBody()
+//void ruleStatBody()
 
 /*  22: <assign> -> <expression> <expression_n>
     23: <assign> -> id ( <params> ) */
-//bool ruleAssign()
+//void ruleAssign()
 
 /*  26: <id_n> -> , id <id_n>
     27: <id_n> -> eps */
-//bool ruleIdN()
+//void ruleIdN()
 
 /*  28: <expression_n> -> , <expression> <expression_n>
     29: <expression_n> -> eps */
-//bool ruleExpressionN()
+//void ruleExpressionN()
 
 /*  33: <return> -> return <return_exp>
     34: <return> -> eps */
-//bool ruleReturn()
+//void ruleReturn()
 
 /*  35: <return_exp> -> <expression> <expression_n>
     36: <return_exp> -> eps */
-//bool ruleReturnExp()
+//void ruleReturnExp()
 
 /*  37: <expression> -> expression */
-//bool ruleExpression()
-
-
+//void ruleExpression()
 
 void parser_main() {
     stack = stackInit();
