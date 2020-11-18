19b39d4278eec63acec4735f4c1dde020ce29811
b3lix <xbelko02@stud.fit.vutbr.cz>
2020-11-18 22:18:41 +0100

    IF-15 Added rules comments

--- parser.c
+++ parser.c
@@ -101,36 +101,63 @@ bool ruleFunc() {
 
 /*  6: <params> -> id <type> <params_n>
     7: <params> -> eps  */
+//bool ruleParams() 
+
 /*  8: <params_n> -> , id <type> <params_n>
     9: <params_n> -> eps */
+//bool ruleParamsN()
+
 /*  10: <type> -> int
     11: <type> -> float64
     12: <type> -> string
     13: <type> -> eps */
+//bool ruleType()
+
 /*  14: <ret_types> -> ( <type> <ret_types_n> )
-    15: <ret_types> -> eps
-    16: <ret_types_n> -> , <type> <ret_types_n>
+    15: <ret_types> -> eps */
+//bool ruleRetTypes()
+
+/*  16: <ret_types_n> -> , <type> <ret_types_n>
     17: <ret_types_n> -> eps */
+//bool ruleRetTypesN()
+
 /*  18: <st_list> -> <stat> EOL <st_list>
-    19: <st_list> -> eps
-    20: <stat> -> id <stat_body>
-    21: <stat_body> -> <id_n> = <assign>
-    22: <assign> -> <expression> <expression_n>
-    23: <assign> -> id ( <params> )
-    24: <stat_body> -> := <expression>
-    25: <stat_body> -> ( <params> )
-    26: <id_n> -> , id <id_n>
-    27: <id_n> -> eps
-    28: <expression_n> -> , <expression> <expression_n>
-    29: <expression_n> -> eps
+    19: <st_list> -> eps */
+//bool ruleStList()
+
+/*  20: <stat> -> id <stat_body>
     30: <stat> -> if <expression> { <st_list> } else { <st_list> }
     31: <stat> -> for <stat> ; <expression> ; <stat> { <st_list> }
-    32: <stat> -> <return>
-    33: <return> -> return <return_exp>
-    34: <return> -> eps
-    35: <return_exp> -> <expression> <expression_n>
-    36: <return_exp> -> eps
-    37: <expression> -> expression */
+    32: <stat> -> <return> */
+//bool ruleStat()
+
+/*  21: <stat_body> -> <id_n> = <assign>
+    24: <stat_body> -> := <expression>
+    25: <stat_body> -> ( <params> ) */
+//bool ruleStatBody()
+
+/*  22: <assign> -> <expression> <expression_n>
+    23: <assign> -> id ( <params> ) */
+//bool ruleAssign()
+
+/*  26: <id_n> -> , id <id_n>
+    27: <id_n> -> eps */
+//bool ruleIdN()
+
+/*  28: <expression_n> -> , <expression> <expression_n>
+    29: <expression_n> -> eps */
+//bool ruleExpressionN()
+
+/*  33: <return> -> return <return_exp>
+    34: <return> -> eps */
+//bool ruleReturn()
+
+/*  35: <return_exp> -> <expression> <expression_n>
+    36: <return_exp> -> eps */
+//bool ruleReturnExp()
+
+/*  37: <expression> -> expression */
+//bool ruleExpression()
 
 
 
--- parser.h
+++ parser.h
@@ -20,5 +20,19 @@ bool ruleProgram();
 bool ruleBody();
 bool ruleFuncN();
 bool ruleFunc();
+//bool ruleParams();
+//bool ruleParamsN();
+//bool ruleType();
+//bool ruleRetTypes();
+//bool ruleRetTypesN();
+//bool ruleStList();
+//bool ruleStat();
+//bool ruleStatBody();
+//bool ruleAssign();
+//bool ruleIdN();
+//bool ruleExpressionN();
+//bool ruleReturn();
+//bool ruleReturnExp();
+//bool ruleExpression();
 
 #endif
