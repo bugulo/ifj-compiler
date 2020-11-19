b2f03a816b890ad7d59a1622134dbde3b82bc9e4
b3lix <xbelko02@stud.fit.vutbr.cz>
2020-11-19 17:42:21 +0100

    IFJ-15 Add functions

--- test.go
+++ test.go
@@ -1,7 +1,14 @@
 package main
 
-func test() {
+func test(test string, test int) (int, string, float64) {
+	if {
+	} else {
+	}
+	return 5 + 5
+
+	for  ; ; {
+		
+	}
 }
 
-func test() {
-}
+func test(test string) {}
