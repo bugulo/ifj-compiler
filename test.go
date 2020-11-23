f01267a025fbae2ddd5fb3e13c021bdd3988f6ef
Michal Šlesár <uarebugged@gmail.com>
2020-11-23 15:07:14 +0100

    IFJ-15 Change test file

--- test.go
+++ test.go
@@ -1,19 +1,10 @@
-
-
-
+//compiler 2
+//
 package main
 
 func main() {
-	test := "safasfs"
-	test2 := 8
-
-	for test := 20; 5 > 5; test = 10 {
-		test, test2 = baf(21)
-	} 
-	
-	return
 }
 
-func baf(test int) (string, int) {
-	return "fsafsa", 5
+func baf(test int) (int) {
+	return 5
 }
